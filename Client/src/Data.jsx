89740bc6003fa05f7bd78e72580f170d7b1d52b6
Piyush Kumar Jha <105655779+Piyush5784@users.noreply.github.com>
2024-02-12 22:30:21 +0530

    error fixes

--- Client/src/Data.jsx
+++ Client/src/Data.jsx
@@ -17,6 +17,7 @@ export const BackendUrl = "https://developercards.onrender.com/";
 export async function getData() {
     const response = await fetch(`${BackendUrl}` + "getPersons")
     const data = await response.json();
+    console.log(data)
     return data;
 }
 
@@ -24,7 +25,8 @@ export async function postData(newPerson) {
 
     try {
         const response = await axios.post(`${BackendUrl}showCart`, { newPerson })
-        console.log(response.data.Message);
+        console.log(response)
+        alert(response.data.Message);
     } catch (error) {
         console.log('Error: ' + error);
     }
